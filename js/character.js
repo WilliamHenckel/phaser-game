e72fb646a84ac5d696fb8579f0941e2efbb0afac
WilliamHenckel <williamhenckel@hotmail.com>
2017-03-26 22:20:21 +0200

    Modif level 4 + fix bug selection du perso

--- js/character.js
+++ js/character.js
@@ -1,22 +1,22 @@
 var characterState = {
   create: function () {
-    game.choice = game.add.text(game.world.centerX, 100, 'Choix du personnage', {font: fontxl, fill: textColor});
+    game.choice = game.add.text(400, 100, 'Choix du personnage', {font: fontxl, fill: textColor});
     game.choice.anchor.set(0.5);
 
-    game.achilleName = game.add.text(game.world.centerX, game.world.centerY + 200, 'Achille', {font: fontxl, fill: textColor});
+    game.achilleName = game.add.text(400, game.world.centerY + 200, 'Achille', {font: fontxl, fill: textColor});
     game.achilleName.anchor.set(0.5);
     game.achilleName.alpha = 0;
 
-    game.ernestName = game.add.text(game.world.centerX, game.world.centerY + 200, 'Ernest', {font: fontxl, fill: textColor});
+    game.ernestName = game.add.text(400, game.world.centerY + 200, 'Ernest', {font: fontxl, fill: textColor});
     game.ernestName.anchor.set(0.5);
     game.ernestName.alpha = 0;
 
-    game.achille = game.add.sprite(game.world.centerX-75, game.world.centerY, 'achilleimg');
+    game.achille = game.add.sprite(400 - 75, game.world.centerY, 'achilleimg');
     game.achille.anchor.set(0.5);
     game.achille.alpha = 0.5;
     game.achille.inputEnabled = true;
 
-    game.ernest = game.add.sprite(game.world.centerX+75, game.world.centerY, 'ernestimg');
+    game.ernest = game.add.sprite(400 + 75, game.world.centerY, 'ernestimg');
     game.ernest.anchor.set(0.5);
     game.ernest.alpha = 0.5;
     game.ernest.inputEnabled = true;
@@ -48,6 +48,6 @@ var characterState = {
       return;
     }
 
-    game.state.start('play', true, false, 1);
+    game.state.start('play', true, false, 4);
   }
 };
